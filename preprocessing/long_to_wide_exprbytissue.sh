1c76c0106cd07c87633eddb9e13c1824ed93d4f9
Victor Wang <xwang145@jhu.edu>
2020-05-15 14:39:51 -0400

    updated infolder and outfolder

--- preprocessing/long_to_wide_exprbytissue.sh
+++ preprocessing/long_to_wide_exprbytissue.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-infolder=/scratch/groups/abattle4/victor/GTExV6PRareVariationData/preprocessing/PEER_gtex8
-#outfolder=/scratch/groups/abattle4/victor/GTExV6PRareVariationData/preprocessing/PEER_gtex8_wide
-outfolder=/scratch/groups/abattle4/jessica/ATG/Final/PEER_gtex8_wide
+infolder=$RAREVARDIR/preprocessing/PEER_gtex8
+outfolder=$RAREVARDIR/preprocessing/PEER_gtex8_wide
+# outfolder=/scratch/groups/abattle4/jessica/ATG/Final/PEER_gtex8_wide
 for file in $(ls ${infolder}/*globalOutliers.v8ciseqtls.removed.outlierGenes.zscores.txt); do
 base=$(basename $file)
 echo ${file}
