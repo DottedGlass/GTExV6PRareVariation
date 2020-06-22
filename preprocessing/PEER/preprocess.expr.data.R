2eb692732f703143b489c176ec699339f6709958
Victor Wang <xwang145@jhu.edu>
2020-06-22 16:31:46 -0400

    different tissue file and filters for percent of individuals

--- preprocessing/PEER/preprocess.expr.data.R
+++ preprocessing/PEER/preprocess.expr.data.R
@@ -15,16 +15,16 @@ require(reshape2)
 
 dir = Sys.getenv('RAREVARDIR')
 peer.dir = paste0(Sys.getenv('PEER_DIR'), '/')
-covs.dir = paste0(dir, '/data/covariates/')
+covs.dir = paste0(dir, '/data/GTEx/GTEx_Analysis_v8_eQTL_covariates/')
 
 # Read in list of tissues with eQTL data
-tissues = read.table('preprocessing/PEER/gtex_eqtl_tissues.txt', header = F, stringsAsFactors = F)[, 1]
+tissues = read.table(paste0(dir,'/reference/gtex_v8_tissues_all_normalized_samples.txt'), header = F, stringsAsFactors = F)[, 1]
 
 # For each tissue, read in the RPKM and covariate files
 # Subset and reorder columns of RPKM file to match covariate file
-# Filter for genes with >= 10 individuals with RPKM > 0.1 and read count > 6
+# Filter for genes with >= 10% of individuals with RPKM > 0.1 and read count > 6
 # Log2 + 2 transform the data, then z-transform
-ind_filt = 10
+ind_filt_per = 0.1
 rpkm_filt = 0.1
 read_filt = 6
 
@@ -32,21 +32,21 @@ for(i in 1:length(tissues)){
 	tissue = tissues[i]
 	print(i)
 	print(tissue)
-	rpkm = as.data.frame(fread(paste0(peer.dir, tissue, '.rpkm.txt'), header = T))
+	rpkm = as.data.frame(fread(paste0(peer.dir, tissue, '.tpm.txt'), header = T))
 	rownames(rpkm) = rpkm[, 1]
 	rpkm = rpkm[, -1]
 	reads = as.data.frame(fread(paste0(peer.dir, tissue, '.reads.txt'), header = T))
 	rownames(reads) = reads[, 1]
 	reads = reads[, -1]
 	reads = reads[rownames(rpkm), names(rpkm)]
-	covariates = read.table(paste0(covs.dir, tissue, '_Analysis.covariates.txt'), header = T, stringsAsFactors = F, row.names = 1)
+	covariates = read.table(paste0(covs.dir, tissue, '.v8.covariates.txt'), header = T, stringsAsFactors = F, row.names = 1)
 	colnames(covariates) = gsub('\\.', '-', colnames(covariates))
 
 	rpkm = rpkm[, colnames(covariates)]
 	reads = reads[, colnames(covariates)]
-	indices_to_keep = rowSums(rpkm > rpkm_filt & reads > read_filt) >= ind_filt
+	indices_to_keep = rowSums(rpkm > rpkm_filt & reads > read_filt) >= ind_filt_per * ncol(rpkm)
 	rpkm = rpkm[indices_to_keep, ]
 	rpkm_out = scale(t(log2(rpkm + 2)))
-	write.table(rpkm_out, paste0(peer.dir, tissue, '.rpkm.log2.ztrans.txt'), quote = F, sep = '\t', row.names = T, col.names = T)
+	write.table(rpkm_out, paste0(peer.dir, tissue, '.tpm.log2.ztrans.txt'), quote = F, sep = '\t', row.names = T, col.names = T)
 }
 
